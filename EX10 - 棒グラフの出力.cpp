2c74108929d7c9edfa2cebd014fc634467d239b3
Jimo-potato <76jirou@gmail.com>
2026-08-27 17:01:16 +0900

    EX10 - 棒グラフの出力.cpp

---
+++ EX10 - 棒グラフの出力.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  // ここにプログラムを追記
+  int i;
+  
+  cout << "A:";
+  for (i = 0; i < A; i++) {
+    cout << "]";
+  }
+  cout << endl;
+  
+  cout << "B:";
+  for (i = 0; i < B; i++) {
+    cout << "]";
+  }
+  cout << endl;
+}
